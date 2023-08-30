0d2f23cd396765d9431876c8669dfcd88ed44480
LarissaHHU <133758934+LarissaHHU@users.noreply.github.com>
2023-08-30 16:31:29 +0200

    Update Python Script TechLabs Group 3.py

--- Python Script TechLabs Group 3.py
+++ Python Script TechLabs Group 3.py
@@ -30,4 +30,25 @@ QOLI_df = pd.merge(QOLI, HE, LE, HS)
 print(QOLI_df)
 
 
+#pandas melt for getting data structured in the way of: country, index_name_year, index_value
+
+QOLI_DF_melt = pd.melt(QOLI_DF, id_vars=['Country'], value_vars=['QOLI_2012', 'QOLI_2013', 'QOLI_2014', 'QOLI_2015', 'QOLI_2016', 'QOLI_2017', 'QOLI_2018', 'QOLI_2019', 'QOLI_2020', 'QOLI_2021', 'QOLI_2022', 'QOLI_2023'], value_name='value', col_level=None, ignore_index=True)
+
+print(QOLI_DF_melt)
+
+#Export the DataFrame to an Excel file
+QOLI_DF_melt.to_excel('QOLI2015_2023.xlsx', index=False)
+
+with pd.ExcelWriter('QOLI2015_2023.xlsx') as excel_writer:
+    #QOLI_DF_melt.to_excel(excel_writer, sheet_name='Sheet1', index=False)
+
+import os
+
+path = r'C:\Users\Pomrehn\Desktop\TechLabs'
+csv_files = [os.path.join(path+"\\", file) for file in os.listdir(path) if file.endswith('.csv')]
+
+QOLI_DF_melt = [pd.read_csv(d) for d in csv_files]
+
+QOLI_DF_melt.to_csv("QOLI2015_2023.csv")
+
 
